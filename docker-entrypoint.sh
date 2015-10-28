6e2a4aae1e60af074b757a7e894580ef46916412
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-10-28 20:31:01 +0100

    New PhpMyAdmin release. Some comments added.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Support for stopping container with `Ctrl+C`
+
 set -e
 
+# Support for UPLOAD_SIZE env var, if specified - will be used instead of default value 128M
+
 test -z $UPLOAD_SIZE || sed -i "s/128M/$UPLOAD_SIZE/g" /var/www/html/.htaccess
+
+# Start Apache as usual
 
 exec apache2-foreground
